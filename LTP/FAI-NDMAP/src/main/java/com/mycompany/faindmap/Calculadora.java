21f3080fdd47feef6051ac0d624e574d54ff0e29
Cezarine <96452545+Cezarine@users.noreply.github.com>
2023-08-07 21:34:37 -0300

    Aula - 01

---
+++ LTP/FAI-NDMAP/src/main/java/com/mycompany/faindmap/Calculadora.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.faindmap;
+
+/**
+ *
+ * @author unifai
+ */
+public class Calculadora {
+    
+    public String cor;
+    public String teclas;
+    public float tamanho;
+    public float peso;
+    public float resultado;
+    
+    public float somar(float valor1, float valor2){
+        this.resultado = valor1 + valor2;
+        return this.resultado;
+    }
+    
+    public float somar(int valor1, int valor2){
+        this.resultado = valor1 + valor2;
+        return this.resultado;
+    }
+    
+    public static void main(String[] args) {
+        Calculadora hp = new Calculadora();
+        Calculadora multilaser = new Calculadora();
+        Calculadora microsoft = new Calculadora();
+        
+        System.out.println(hp.somar(5, 6));
+        System.out.println(multilaser.somar(5.24f, 6.56f));
+    }
+}
---
+++ LTP/FAI-NDMAP/src/main/java/com/mycompany/faindmap/Grafos.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.faindmap;
+
+/**
+ *
+ * @author unifai
+ */
+public class Grafos {
+    public static void main(String[] args) {
+        System.out.println("Olá Grafos");
+    }
+}
