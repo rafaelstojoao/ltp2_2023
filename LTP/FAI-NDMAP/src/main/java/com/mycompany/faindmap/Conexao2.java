87271d1adfa79a8e9ffbbd84fcef33562eada8fd
Cezarine <96452545+Cezarine@users.noreply.github.com>
2023-10-23 21:57:54 -0300

    ajustes

--- LTP/FAI-NDMAP/src/main/java/com/mycompany/faindmap/Conexao2.java
+++ LTP/FAI-NDMAP/src/main/java/com/mycompany/faindmap/Conexao2.java
@@ -4,28 +4,38 @@
  */
 package com.mycompany.faindmap;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 /**
  *
  * @author unifai
  */
 public class Conexao2 {
-    String url = "jdbc:mysql://localhost:3306/dbfaindmap";
-    private final String user = "sistema";
-    private final String senha = "498123498";
+    private String user;
+    private String password;
+    private String database;
+    private String port;
+    private String server;
+    private String url;
     
     public Connection con = null;
     public Statement stmt = null;
     public ResultSet res  = null;
     
     public void Conecta(){
-        try{          
-            this.con = DriverManager.getConnection(this.url, this.user, this.senha);
+        try{ 
+            ReadINI();
+            this.con = DriverManager.getConnection(this.url, this.user, this.password);
             stmt = con.createStatement();
             System.out.println("Conectado");
         }
@@ -33,4 +43,20 @@ public class Conexao2 {
             System.out.println(e.getMessage());
         }
     }
+    
+    private void ReadINI(){
+        Properties prop = new Properties();
+        try{
+            prop.load(new FileInputStream("config.ini"));
+            user        = prop.getProperty("User");
+            password    = prop.getProperty("Password");
+            database    = prop.getProperty("Database");
+            port        = prop.getProperty("Port"); 
+            server      = prop.getProperty("Server");       
+            url         = prop.getProperty("Url") + server + ":" + port + "/" + database;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }     
 }
+
